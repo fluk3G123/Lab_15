9878f393eb2c872bc38b9c06900659e71645a6c6
__.flxk.__ <89276072+fluk3G123@users.noreply.github.com>
2023-02-13 21:00:05 +0700

    T.T

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+    for (int position = 1 ; position < N ; position++)
+	{
+		cout << "Pass " << position << ":";
+		int F = position-1;
+		T box = d[position];
+		while (d[F] < box && F >= 0)
+		{
+			d[F+1] = d[F];
+			F--;
+		}
+		d[F+1] = box;
+		for(int j=0;j < N;j++){  
+			cout << d[j] ; 
+			if(j!=N-1) cout << " "; 
+		}
+		cout << endl;
+	}
 }
 
 int main(){
